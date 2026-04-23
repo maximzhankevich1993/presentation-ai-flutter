bbaabcba64fde4b9a8f4c2d9712b271c73fe4429
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-04-23 11:07:04 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
+import 'dart:io';
+
+import 'providers/theme_provider.dart';
+import 'providers/user_provider.dart';
+import 'screens/home_screen.dart';
+import 'themes/app_theme.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Настройка окна для десктопа
+  if (Platform.isWindows || Platform.isMacOS) {
+    await windowManager.ensureInitialized();
+    await windowManager.setMinimumSize(const Size(500, 700));
+    await windowManager.setSize(const Size(600, 900));
+    await windowManager.center();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        builder: (_, child) {
+          return MaterialApp(
+            title: 'Презентатор ИИ',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: ThemeMode.system,
+            home: const HomeScreen(),
+          );
+        },
+      ),
+    );
+  }
+}
