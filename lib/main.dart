a2b8be491f7adc61b9d467e706e7ff03def28dfe
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-05-04 20:59:27 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,12 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-import 'package:window_manager/window_manager.dart';
-import 'dart:io';
-
 import 'providers/theme_provider.dart';
 import 'providers/user_provider.dart';
 import 'screens/home_screen.dart';
 import 'themes/app_theme.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Desktop window setup
-  if (kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isLinux)) {
-    await windowManager.ensureInitialized();
-
-    WindowOptions windowOptions = const WindowOptions(
-      size: Size(600, 900),
-      minimumSize: Size(500, 700),
-      center: true,
-      title: 'Презентатор ИИ',
-    );
-
-    windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await windowManager.show();
-      await windowManager.focus();
-    });
-  }
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -46,7 +23,7 @@ class MyApp extends StatelessWidget {
       child: ScreenUtilInit(
         designSize: const Size(375, 812),
         minTextAdapt: true,
-        builder: (context, _) {
+        builder: (_, child) {
           return MaterialApp(
             title: 'Презентатор ИИ',
             debugShowCheckedModeBanner: false,
