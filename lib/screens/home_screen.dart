dff4b44c463fa13d933c91e8487b7f0c2a2c3733
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-05-13 21:48:41 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import '../providers/user_provider.dart';
-import '../providers/logo_provider.dart';  // ← Оставить только здесь
 import 'loading_screen.dart';
 import 'premium_screen.dart';
 import 'settings_screen.dart';
