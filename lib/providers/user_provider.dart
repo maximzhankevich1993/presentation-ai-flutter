4c958886b981be3ca7a848a654d93fa50b1a3449
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-05-13 22:15:23 +0300

    Update user_provider.dart

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,32 +1,60 @@
 import 'package:flutter/material.dart';
 
+class UserModel {
+  final String name;
+  final String email;
+  final String? avatarUrl;
+
+  UserModel({
+    required this.name,
+    required this.email,
+    this.avatarUrl,
+  });
+}
+
 class UserProvider extends ChangeNotifier {
   String _userName = 'User';
   String _userEmail = '';
+
+  String? _avatarUrl;
 
   bool _isPremium = false;
   bool _isLoggedIn = true;
 
   int _freeGenerationsLeft = 5;
 
+  // USER OBJECT
+  UserModel get user => UserModel(
+        name: _userName,
+        email: _userEmail,
+        avatarUrl: _avatarUrl,
+      );
+
   // GETTERS
   String get userName => _userName;
   String get userEmail => _userEmail;
+  String? get avatarUrl => _avatarUrl;
 
   bool get isPremium => _isPremium;
   bool get isLoggedIn => _isLoggedIn;
 
   int get freeGenerationsLeft => _freeGenerationsLeft;
 
-  // USERNAME
+  // SET USERNAME
   void setUserName(String name) {
     _userName = name;
     notifyListeners();
   }
 
-  // EMAIL
+  // SET EMAIL
   void setUserEmail(String email) {
     _userEmail = email;
+    notifyListeners();
+  }
+
+  // SET AVATAR
+  void setAvatarUrl(String? url) {
+    _avatarUrl = url;
     notifyListeners();
   }
 
@@ -40,17 +68,21 @@ class UserProvider extends ChangeNotifier {
   void login({
     required String name,
     required String email,
+    String? avatarUrl,
     bool premium = false,
   }) {
     _userName = name;
     _userEmail = email;
+
+    _avatarUrl = avatarUrl;
+
     _isPremium = premium;
     _isLoggedIn = true;
 
     notifyListeners();
   }
 
-  // USE FREE GENERATION
+  // FREE GENERATIONS
   void useFreeGeneration() {
     if (_freeGenerationsLeft > 0) {
       _freeGenerationsLeft--;
@@ -58,7 +90,6 @@ class UserProvider extends ChangeNotifier {
     }
   }
 
-  // RESET FREE GENERATIONS
   void resetFreeGenerations() {
     _freeGenerationsLeft = 5;
     notifyListeners();
@@ -68,6 +99,8 @@ class UserProvider extends ChangeNotifier {
   void logout() {
     _userName = 'User';
     _userEmail = '';
+
+    _avatarUrl = null;
 
     _isPremium = false;
     _isLoggedIn = false;
