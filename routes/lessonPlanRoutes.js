33b394acd5243f7d57a7e265048c0458a76d157c
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-05-18 21:35:33 +0300

    Delete lessonPlanRoutes.js

--- routes/lessonPlanRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateLessonPlan } = require('../controllers/lessonPlanController');
-
-router.post('/generate', generateLessonPlan);
-
-module.exports = router;
