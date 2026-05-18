446c541c854f405bdbb8517a9541a0d4e0557f43
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-05-18 10:51:52 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'providers/user_provider.dart';
 import 'providers/logo_provider.dart';
 import 'providers/history_provider.dart';
 import 'screens/home_screen.dart';
-import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,55 +30,8 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Inter',
           useMaterial3: true,
         ),
-        home: const AuthWrapper(),
+        home: const HomeScreen(),  // ← Всегда HomeScreen
       ),
     );
-  }
-}
-
-// Обёртка для загрузки пользователя
-class AuthWrapper extends StatefulWidget {
-  const AuthWrapper({super.key});
-
-  @override
-  State<AuthWrapper> createState() => _AuthWrapperState();
-}
-
-class _AuthWrapperState extends State<AuthWrapper> {
-  bool _isLoading = true;
-  bool _isAuthenticated = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkAuth();
-  }
-
-  Future<void> _checkAuth() async {
-    final userProvider = context.read<UserProvider>();
-    
-    // Загружаем пользователя из токена
-    await userProvider.loadUser();
-    
-    setState(() {
-      _isAuthenticated = userProvider.isLoggedIn;
-      _isLoading = false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_isLoading) {
-      return const Scaffold(
-        backgroundColor: Color(0xFF121212),
-        body: Center(
-          child: CircularProgressIndicator(
-            color: Color(0xFF1DB954),
-          ),
-        ),
-      );
-    }
-    
-    return _isAuthenticated ? const HomeScreen() : const LoginScreen();
   }
 }
